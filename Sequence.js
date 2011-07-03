ffbf4c31c8d417f914bcef902fda515aa7bde68a
Nicolas Silva <nical.silva@gmail.com>
2011-07-03 16:07:25 +0200

    added support for chromium

--- Sequence.js
+++ Sequence.js
@@ -10,7 +10,7 @@ Sequence.prototype.update = function(dt) {
 	{
 		this.itemTime -= this.data[this.currentItem].duration
 		this.currentItem++
-		
+
 		if (this.currentItem >= this.data.length)
 			this.currentItem = 0
 	}
@@ -18,28 +18,28 @@ Sequence.prototype.update = function(dt) {
 
 Sequence.prototype.draw = function(mvpMatrix, beat) {
 	var item = this.data[this.currentItem]
-	
+
 	item.shader.bind()
-	
+
 	var positionAttribute = item.shader.getAttribute("pos1");
 	gl.enableVertexAttribArray(positionAttribute)
-	
+
 	var positionAttribute2 = item.shader.getAttribute("pos2");
 	gl.enableVertexAttribArray(positionAttribute2)
-	
+
 	/*var colorAttribute = item.shader.getAttribute("color");
 	if ("color" in item)
 		gl.enableVertexAttribArray(colorAttribute)
 	else
 		gl.disableVertexAttribArray(colorAttribute)*/
-	
+
 	var mvpUniform = item.shader.getUniform("mvpMatrix")
 	gl.uniformMatrix4fv(mvpUniform, false, mvpMatrix);
-	
+
 	var localTime = this.itemTime / item.duration;
 	var timeUniform = item.shader.getUniform("time")
 	gl.uniform1f(timeUniform, localTime);
-	
+
 	var size = Math.min(item.mesh1.vbo.itemSize, item.mesh2.vbo.itemSize);
 	gl.bindBuffer(gl.ARRAY_BUFFER, item.mesh1.vbo);
 	gl.vertexAttribPointer(positionAttribute, size, gl.FLOAT, false, 0, 0);
@@ -50,9 +50,12 @@ Sequence.prototype.draw = function(mvpMatrix, beat) {
 		gl.bindBuffer(gl.ARRAY_BUFFER, item.color.vbo);
 		gl.vertexAttribPointer(colorAttribute, item.color.vbo.itemSize, gl.FLOAT, false, 0, 0);
 	}*/
-	
+
 	for(var i = 0; i < 32; i++) {
-		var v = vu.getLevel(i);
+		var v = undefined;
+    if(onFirefox){
+      v = vu.getLevel(i);
+    }
 		//console.log("band " + i + ": " + v);
 		var fftUniform = item.shader.getUniform("fft[" + i + "]");
 		if (v != undefined)
@@ -60,7 +63,7 @@ Sequence.prototype.draw = function(mvpMatrix, beat) {
 		else
 			gl.uniform1f(fftUniform, 1.0);
 	}
-	
+
 	gl.drawArrays(gl.POINTS, 0, item.mesh1.vbo.numItems);
 }
 
--- main.js
+++ main.js
@@ -1,3 +1,8 @@
+var thisBrowser = navigator.userAgent.toLowerCase();
+var onFirefox = (thisBrowser.indexOf("firefox") != -1);
+
+alert(onFirefox)
+
 var seq;
 var back;
 var post;
@@ -21,29 +26,29 @@ var light_test;
 
 var bufferSize = 0;
 var signal = new Float32Array(bufferSize);
-var channels = 0;   
-var rate = 0;   
+var channels = 0;
+var rate = 0;
 var frameBufferLength = 0;
 var fft = null;
 
 function initDemo() {
 	var audioElement = document.getElementById("music")
 	audioElement.play()
-	
+
 	initContext();
-	
+
 	// create GL buffers
 	for (meshName in meshArrays)
 	{
 		meshes[meshName] = new MeshBuffer(meshArrays[meshName])
 	}
-	
+
 	// offscreen render target
 	/*sceneBuffer = gl.createFramebuffer()
 	gl.bindFramebuffer(gl.FRAMEBUFFER, sceneBuffer)
 	sceneBuffer.width = gl.viewportWidth
 	sceneBuffer.height = gl.viewportHeight
-	
+
 	sceneTexture = gl.createTexture()
 	gl.bindTexture(gl.TEXTURE_2D, sceneTexture)
 	gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.LINEAR)
@@ -53,13 +58,13 @@ function initDemo() {
 	gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGBA, sceneBuffer.width, sceneBuffer.height, 0, gl.RGBA, gl.UNSIGNED_BYTE, null)
 	gl.framebufferTexture2D(gl.FRAMEBUFFER, gl.COLOR_ATTACHEMENT0, gl.TEXTURE_2D, sceneTexture, 0)
 	gl.bindTexture(gl.TEXTURE_2D, null)
-	
+
 	sceneDepth = gl.createRenderbuffer()
 	gl.bindRenderbuffer(gl.RENDERBUFFER, sceneDepth)
 	gl.renderbufferStorage(gl.RENDERBUFFER, gl.DEPTH_COMPONENT16, sceneDepth, sceneBuffer.width, sceneBuffer.height)
 	gl.framebufferRenderbuffer(gl.FRAMEBUFFER, gl.DEPTH_ATTACHEMENT, gl.RENDERBUFFER, sceneDepth)
 	gl.bindRenderbuffer(gl.RENDERBUFFER, null)*/
-	
+
 	// main particle sequence
 	var sequenceData = [
 		{
@@ -76,8 +81,8 @@ function initDemo() {
 		},
 		{
 			duration: 4000,
-			mesh2: meshes.monkey,
-			mesh1: meshes.wtfMonkey,
+			mesh1: meshes.monkey,
+			mesh2: meshes.wtfMonkey,
 			shader: new ShaderProgram("vs", "fs")
 		},
 		{
@@ -116,8 +121,8 @@ function initDemo() {
 			mesh2: meshes.demojs,
 			shader: new ShaderProgram("explode-vs", "explode-fs")
 		},
-		
-		// 1:20	
+
+		// 1:20
 		{
 			duration: 26000,
 			mesh1: meshes.credits,
@@ -130,7 +135,7 @@ function initDemo() {
 			mesh2: meshes.credits,
 			shader: new ShaderProgram("gravity-vs", "gravity-fs")
 		},
-		
+
 		// 1:50
 		{
 			duration: 2000,
@@ -162,7 +167,7 @@ function initDemo() {
 			mesh2: meshes.wtfMonkey,
 			shader: new ShaderProgram("explode-vs", "explode-fs")
 		},
-		
+
 		// 2:00
 		{
 			duration: 16000,
@@ -194,7 +199,7 @@ function initDemo() {
 			mesh2: meshes.sadMonkey,
 			shader: new ShaderProgram("vs", "fs")
 		},
-		
+
 		// 2:44
 		{
 			duration: 40000,
@@ -203,15 +208,15 @@ function initDemo() {
 			shader: new ShaderProgram("gravity-vs", "gravity-fs")
 		},
 	]
-	
+
 	back = new Background()
-	
+
 	// create initial sequences
 	seq = new Sequence(sequenceData)
-	
+
 	// post processor
 	post = new PostProcess()
-	
+
 	// start rendering
 	requestAnimFrame(render)
 }
@@ -237,10 +242,10 @@ function initGL(canvas) {
 function initContext() {
 	var canvas = document.getElementById("renderCanvas");
 	initGL(canvas);
-	
+
 	gl.clearColor(0.0, 0.0, 0.0, 1.0);
 	//gl.enable(gl.DEPTH_TEST);
-	
+
 	gl.enable(gl.BLEND);
 	gl.blendFunc(gl.ONE, gl.ONE);
 }
@@ -254,18 +259,20 @@ function onLoadedMetadata()
 	frameBufferLength = audio.mozFrameBufferLength;
 
 	bufferSize = frameBufferLength/channels;
+  if(onFirefox){
+    fft = new FFT(bufferSize, rate);
+    signal = new Float32Array(bufferSize);
 
-	fft = new FFT(bufferSize, rate);
-	signal = new Float32Array(bufferSize);
-
-	audio.addEventListener("MozAudioAvailable",audioWritten,false);				
+	audio.addEventListener("MozAudioAvailable",audioWritten,false);
 	bd = new BeatDetektor(80, 159);
-	kick_det = new BeatDetektor.modules.vis.BassKick();    
+	kick_det = new BeatDetektor.modules.vis.BassKick();
 	vu = new BeatDetektor.modules.vis.VU();
+  }
 }
 
-function audioWritten(event) 
+function audioWritten(event)
 {
+  if(!onFirefox) return;
 	if (fft == null) return;
 
 	var fb = event.frameBuffer;
@@ -315,27 +322,27 @@ function audioWritten(event)
 function render()
 {
 	var currentTime = new Date().getTime()
-	
+
 	var dt = currentTime - time;
 	time = currentTime
 	globalTime += dt
 	if (dt > 500)
 		dt = 500
-	
+
 	back.update(dt)
 	seq.update(dt)
 	post.update(dt)
-	
+
 	// offscreen pass
 	/*gl.bindTexture(gl.TEXTURE_2D, null)
 	gl.bindFramebuffer(gl.FRAMEBUFFER, sceneBuffer)
 	alert(gl.checkFramebufferStatus(gl.FRAMEBUFFER))*/
-	
+
 	gl.clearColor(0.0, 0.0, 0.0, 1.0)
 	gl.clear(gl.COLOR_BUFFER_BIT)
-	
+
 	gl.disable(gl.BLEND);
-	
+
 	var magic = 0.0;
 	if (globalTime < 16000)
 		magic = globalTime / 16000.0;
@@ -343,19 +350,20 @@ function render()
 		magic = 3.0 - 0.002 * (globalTime - 16000.0);
 	else if (globalTime < 48000)
 		magic = 1.0;
-	else if (globalTime < 80000)
-		magic = vu.getLevel(5);
-	else if (globalTime < 110000)
-		magic = vu.getLevel(8) * 3.0;
-	back.draw(gl.viewportWidth / gl.viewportHeight, magic)
-	
+	else if (globalTime < 80000){
+		if(onFirefox) magic = vu.getLevel(5);
+  }else if (globalTime < 110000){
+    if(onFirefox) magic = vu.getLevel(8) * 3.0;
+  }
+  back.draw(gl.viewportWidth / gl.viewportHeight, magic)
+
 	gl.enable(gl.BLEND);
-	
+
 	var mvMatrix = mat4.create()
 	var pMatrix = mat4.create()
 	var mvpMatrix = mat4.create()
 	mat4.identity(mvMatrix);
-	
+
 	if (globalTime < 16000)
 		mat4.lookAt([0,15, 10],[0,0,0],[0,0,1], mvMatrix);
 	else if (globalTime < 48000)
@@ -364,30 +372,30 @@ function render()
 		mat4.lookAt([20.0*Math.cos(globalTime * 0.001),20.0*Math.sin(globalTime * 0.0001), 10 * Math.sin(0.0001*globalTime) + 2],[0,0,2],[0,0,1], mvMatrix);
 	else
 		mat4.lookAt([20.0*Math.cos(globalTime * 0.00008),20.0*Math.sin(globalTime * 0.0001), 10 * Math.sin(0.0001*globalTime) + 2],[0,0,2],[0,0,1], mvMatrix);
-	
+
 	//mat4.lookAt([10*Math.cos(time * 0.0001),15*Math.sin(time * 0.0001), 10+Math.sin(0.0001*time) * 1],[0,0,0],[0,0,1], mvMatrix);
 	//mat4.lookAt([5*Math.cos(0),5*Math.sin(0), 1+Math.sin(0.0001*time) * 3],[0,0,0],[0,0,1], mvMatrix);
 	mat4.perspective(45, gl.viewportWidth / gl.viewportHeight, 0.1, 100.0, pMatrix);
 	mat4.multiply(pMatrix, mvMatrix, mvpMatrix)
-	
+
 	//var beat = Math.exp(Math.sin(time))
 	var beat = 0.0;
 	if (globalTime > 16000)
 	{
 		seq.draw(mvpMatrix, beat)
 	}
-	
-	if (globalTime > 195000)
-		alert("Thanks for watching!");
-	
+
+	if (globalTime > 195000){
+		//alert("Thanks for watching!");
+  }
 	// post processing
 	/*gl.bindFramebuffer(gl.FRAMEBUFFER, null)
 	gl.activeTexture(gl.TEXTURE0)
 	gl.bindTexture(gl.TEXTURE_2D, sceneTexture)
-	
+
 	gl.disable(gl.BLEND)
 	post.draw(gl.viewportWidth / gl.viewporteight)*/
-	
+
 	requestAnimFrame(render)
 }
 
